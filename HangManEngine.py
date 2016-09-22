fc281501247884fd61211df2767d6c2bd57d3c2d
sistemi <sistemi@sistemi.(none)>
2016-09-22 16:16:50 +0200

    HangmanEngineprova

--- HangManEngine.py
+++ HangManEngine.py
@@ -2,15 +2,16 @@ __author__ = 'Thomas Hauth, Martin Heck'
 
 class HangManEngine:
   def __init__(self, secretWord):
-    pass
+    self.userInput = None
+    self.secretword = secretWord
 
   def getMessage(self):
     return\
     'This is a game of hangman. For an explanation, please search the web.'
 
-  def readInput(self, choosenInput=None):
-    if choosenInput:
-        userInput = raw_input("Please insert your letter:")
+  def readInput(self, chosenInput=None):
+    if chosenInput is None:
+        self.userInput = input("Please insert your letter: ")
     else:
-	self.userInput = choosenInput
+        self.userInput = chosenInput
     return self.userInput
